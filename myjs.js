c4e37f6e44838a6593052369026d1b1f261b8211
kubotashinn <131092076+kubotashinn@users.noreply.github.com>
2023-07-11 11:08:24 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,13 +1,11 @@
 alert('Hello,shin!');
 alert(1+1);
-alert(Math.sqrt(1+1))
-alert(2+3);
+alert(Math.sqrt(1+1));
 alert(Math.sqrt(2+3));
+alert(1/0);
+alert(0/0)
+alert(2+3);
 alert('2'+'3');
-alert(Math.sqrt('2'+'3'));
-alert(Number('2');Number('3'));
-// alert(Math.sqrt(Number('2')+Number(7#')));
-alert('2'+'3');
-alert(Math.sqrt('2'+'3));
+alert(Number('2')+Number('3'));
+alert(Number('2'+3);
 alert(Number('2')+3);
-alert(Math.sqrt(Number('2')+3));
